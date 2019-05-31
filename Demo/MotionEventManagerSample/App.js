74455c8f095ee8f8e2ba6e8cd52967380f2f91f9
Michael Bai <bailu1234@gmail.com>
2019-05-31 15:51:02 +0800

    Update Demo

--- Demo/MotionEventManagerSample/App.js
+++ Demo/MotionEventManagerSample/App.js
@@ -7,7 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Animated} from 'react-native';
+import { DeviceMotionModule } from 'react-native-motion-event-manager';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,9 +19,38 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      roll: new Animated.Value(0),
+    }
+  }
+
+  componentDidMount() {
+    // replace with your mapping here
+      let eventMapping = {
+          attitude: {
+              roll: this.state.roll, 
+          }
+      };
+      DeviceMotionModule.startDeviceMotionUpdates(eventMapping);
+  }
+
+  componentWillUnmount() {
+      DeviceMotionModule.stopDeviceMotionUpdates();
+  }
+
   render() {
     return (
       <View style={styles.container}>
+      <Animated.Text
+        style={{opacity:this.state.roll.interpolate({
+            inputRange: [-1.5, 1.5],
+            outputRange: [0, 1]
+          })}}>
+        Opacity will change while rolling.
+      </Animated.Text>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
